7027bec389eef58a181a177a9192595c36bdeb1d
Aravindh Sridharan <mail@aravindh.io>
2018-10-14 18:57:51 +0200

    Added yaml parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+extern crate yaml_rust;
+use std::fs::File;
+use std::io::prelude::*;
+use yaml_rust::YamlLoader;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        panic!("Missing Mandatory file");
+    }
+    let config_file = &args[1];
+    let mut file = File::open(config_file).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let yaml = YamlLoader::load_from_str(&contents).unwrap();
+    println!("{:?}", yaml);
 }
