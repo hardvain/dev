6015d1066de94f9dc303e2b35446be09670e71f4
Aravindh Sridharan <mail@aravindh.io>
2018-10-14 19:31:38 +0200

    Executed first command

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,28 @@
 extern crate yaml_rust;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process::Command;
 use yaml_rust::YamlLoader;
 
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     if args.len() < 2 {
-        panic!("Missing Mandatory file");
+        panic!("Missing command");
     }
-    let config_file = &args[1];
+    let config_file = "/Users/aravindhs/Aravindh/projects/dev/dev/dev.yml";
     let mut file = File::open(config_file).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
-    let yaml = YamlLoader::load_from_str(&contents).unwrap();
-    println!("{:?}", yaml);
+    let docs = YamlLoader::load_from_str(&contents).unwrap();
+
+    let doc = &docs[0];
+    let alias = &doc["dev"][args[1].as_str()];
+    let command = &alias["command"].as_str().unwrap();
+    let arguments: &yaml_rust::Yaml = &alias["args"].clone().to_owned();
+    let mut cmd = Command::new(command);
+    for argument in arguments.as_vec().unwrap() {
+        cmd.arg(argument.as_str().unwrap());
+    }
+    let output = cmd.output().expect("error");
+    println!("{:?}", String::from_utf8_lossy(&output.stdout));
 }
